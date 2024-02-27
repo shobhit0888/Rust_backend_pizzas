db498de2c4ef928d85a4bcd5e60a47eaaf14e503
Chhavi Gupta <chhavigupta1705@gmail.com>
2024-02-27 23:51:33 +0530

    phase 2 done

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
-use actix_web::{get, patch, post, web::Json, App, HttpResponse, HttpServer, Responder};
-use validator::Validate;
+// use std::fmt::format;
 
-use crate::models::BuyPizzaRequest; // Importing the required modules
-mod models; // Importing the models module
+use actix_web::web::Data;
+use actix_web::{web::Path, get, patch, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use surrealdb::sql::Uuid;
+use validator::Validate;
+use crate::db::Database;
+use uuid;
+use crate::models::{BuyPizzaRequest, UpdatePizzaUrl,Pizza}; // Importing the required modules
+mod models;
+
+mod db ;
+mod error;
 
 #[get("/pizzas")]
-
-async fn get_pizzas() -> impl Responder {
-    HttpResponse::Ok().body("Pizza are available")
+async fn get_pizzas(db:Data<Database>) -> impl Responder {
+    let pizzas = db.get_all_pizzas().await;
+    match pizzas {
+        Some(found_pizzas) => HttpResponse::Ok().body(format!("{:?}", found_pizzas)),
+        None => HttpResponse::Ok().body("No pizzas found")
+    }
+    // HttpResponse::Ok().body("Pizzas")
 }
 
 #[post("/buypizza")]
-async fn buy_pizza(body:Json<BuyPizzaRequest>) -> impl Responder {
+async fn buy_pizza(body:Json<BuyPizzaRequest>, db: Data<Database>) -> impl Responder {
     let is_valid = body.validate();
     match is_valid {
         Ok(_) => {
             let pizza_name = body.pizza_name.clone();
-            return HttpResponse::Ok().body(format!("Pizza bought125: {}", pizza_name));
-            // println!("Validated");
+            let mut buffer =uuid::Uuid::encode_buffer();
+            let new_uuid=uuid::Uuid::new_v4().simple().encode_lower(&mut buffer);
+            let new_pizza = db.add_pizza(Pizza::new(String::from(new_uuid), pizza_name)).await;
+            match new_pizza{
+                Some(created)=>{
+                    HttpResponse::Ok().body(format!("Pizza bought: {:?}", created))
+
+                
+                },
+                None=>
+                    HttpResponse::Ok().body("Error buying pizza")
+            }
         }
+            //  HttpResponse::Ok().body(format!("Pizza bought125: {}", pizza_name))
+            // println!("Validated");
+        
         Err(_) =>  HttpResponse::Ok().body("Pizza name required"),
             // println!("Error: {}", e);
         
@@ -28,14 +53,18 @@ async fn buy_pizza(body:Json<BuyPizzaRequest>) -> impl Responder {
 }
 
 #[patch("/updatepizza/{uuid}")]
-async fn update_pizza() -> impl Responder {
-    HttpResponse::Ok().body("Pizza updated")
+async fn update_pizza(update_pizza_url:Path<UpdatePizzaUrl>) -> impl Responder {
+    let uuid = update_pizza_url.into_inner().uuid.clone();
+    HttpResponse::Ok().body(format!("Update pizza with uuid: {}", uuid))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let db = Database::init().await.expect("Database connection failed");
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
         App::new()
+        .app_data(db_data.clone())
             .service(get_pizzas)
             .service(buy_pizza)
             .service(update_pizza)
--- src/models/pizza.rs
+++ src/models/pizza.rs
@@ -9,3 +9,26 @@ pub struct BuyPizzaRequest {
     // #[validate(range(min = 1, max = 5))]
     // pub quantity: i32,
 }
+
+#[derive(Validate, Serialize, Deserialize)]
+pub struct UpdatePizzaUrl {
+    #[validate(length(min = 3,message = "uuid should be at least 3 characters"))]
+    pub uuid: String,
+}
+
+#[derive(Validate, Serialize, Deserialize,Debug)]
+
+pub struct Pizza {
+    #[validate(length(min = 3,message = "Name should be at least 3 characters"))]
+    pub uuid: String,
+    pub pizza_name: String,
+   
+}
+impl  Pizza {
+    pub fn new(uuid: String, pizza_name: String) -> Pizza {
+        Pizza {
+            uuid,
+            pizza_name,
+        }
+    }
+}
